3a143fa3df0effae63d63ea684c4159daa1265a7
Omar Pervez <70445442+omarpervezz@users.noreply.github.com>
2022-07-31 18:17:27 +0600

    I have done the work

--- js/custom js/app.js
+++ js/custom js/app.js
@@ -5,85 +5,87 @@ const resetImg = document.querySelector('.reset-item img');
 const incrementValue = document.querySelector('.increment');
 const decrementValue = document.querySelector('.decrement');
 const resetValue = document.querySelector('.reset-item');
-const currentValue = document.querySelectorAll('.result-output'), topValue = document.querySelector('.top-value'), mainBox = document.querySelector('.main-box');
+const currentValue = document.querySelectorAll('.result-output'),
+   topValue = document.querySelector('.top-value'),
+   mainBox = document.querySelector('.main-box');
 const changeTheme = document.querySelector('#changeTheme');
-let initialValue = 0, white = '#fff', black = '#000', emptyItem = '';
+let initialValue = 0,
+   white = '#fff',
+   black = '#000',
+   emptyItem = '';
 // change image src when a user hover on
 // change image src when a user hover out
-function changeImg(){
-  subtraction.addEventListener('mouseover', (event) => {
-    hoverImage(event, subtraction);
-  });
-  subtraction.addEventListener('mouseleave', (event) => {
-   hoverOut(event, subtraction)
-  });
+function changeImg() {
+   subtraction.addEventListener('mouseover', (event) => {
+      hoverImage(event, subtraction);
+   });
+   subtraction.addEventListener('mouseleave', (event) => {
+      hoverOut(event, subtraction)
+   });
 
-  addition.addEventListener('mouseover', (event) => {
-    hoverImage(event,addition);
-  });
-  addition.addEventListener('mouseleave', (event) => {
-    hoverOut(event, addition)
+   addition.addEventListener('mouseover', (event) => {
+      hoverImage(event, addition);
+   });
+   addition.addEventListener('mouseleave', (event) => {
+      hoverOut(event, addition)
    });
    resetImg.addEventListener('mouseover', (event) => {
-    hoverImage(event, resetImg);
+      hoverImage(event, resetImg);
    })
    resetImg.addEventListener('mouseleave', (event) => {
-    hoverOut(event, resetImg);
+      hoverOut(event, resetImg);
    })
 
 
 }
 // hover in
-const hoverImage = (event,element) => {
-// checking which element user hovered
-if(element.alt.includes('subtraction')){
-   event.path[0].src = './assets/subtractbtnpress.png';
-   
-}
-else if(element.alt.includes('reset')){
-  event.path[0].src = './assets/resetbtnpress.png';
-}
-else{
- event.path[0].src = './assets/addbtnpress.png'
-}
+const hoverImage = (event, element) => {
+   // checking which element user hovered
+   if (element.alt.includes('subtraction')) {
+      event.path[0].src = './assets/subtractbtnpress.png';
+
+   } else if (element.alt.includes('reset')) {
+      event.path[0].src = './assets/resetbtnpress.png';
+   } else {
+      event.path[0].src = './assets/addbtnpress.png'
+   }
 }
 // hover out
-const hoverOut = (event,element) => {
-    console.dir(element);
-    if(element.alt.includes('subtraction')){
+const hoverOut = (event, element) => {
+   console.dir(element);
+   if (element.alt.includes('subtraction')) {
       event.path[0].src = './assets/subtractbtn.png';
-    }else if(element.alt.includes('reset')){
+   } else if (element.alt.includes('reset')) {
       event.path[0].src = './assets/resetbtn.png';
-    }
-    else{
+   } else {
       event.path[0].src = './assets/addbtn.png';
       event.path[0].style.height = '100%';
-    }
+   }
 }
 
 
-function changeOutPut(initvalue){
-  for(let x = 0; x < currentValue.length; x++){
-    currentValue[x].innerText = initvalue;
+function changeOutPut(initvalue) {
+   for (let x = 0; x < currentValue.length; x++) {
+      currentValue[x].innerText = initvalue;
    }
 }
 
 // increment current value
 incrementValue.addEventListener('click', () => {
    navigator.vibrate(1000);
-  initialValue++;
-  changeOutPut(initialValue)
+   initialValue++;
+   changeOutPut(initialValue)
 })
 decrementValue.addEventListener('click', () => {
-  navigator.vibrate(1000);
-  initialValue--;
-  initialValue = initialValue < 0 || !initialValue || initialValue === Infinity ? 0 : initialValue;
-  changeOutPut(initialValue)
+   navigator.vibrate(1000);
+   initialValue--;
+   initialValue = initialValue < 0 || !initialValue || initialValue === Infinity ? 0 : initialValue;
+   changeOutPut(initialValue)
 })
 
 resetValue.addEventListener('click', () => {
-  initialValue = 0;
-  changeOutPut(initialValue);
+   initialValue = 0;
+   changeOutPut(initialValue);
    topValue.innerText = '';
 })
 
@@ -91,22 +93,14 @@ resetValue.addEventListener('click', () => {
 // change theme 
 changeTheme.addEventListener('click', () => {
    const body = document.querySelector('body');
-   console.dir(body);
-   if(body.style.backgroundColor == emptyItem && changeTheme.style.backgroundColor == emptyItem && changeTheme.style.color == emptyItem && mainBox.style.background == emptyItem){
-    body.style.backgroundColor = white;
-    mainBox.style.backgroundImage = `linear-gradient(#000000db,#057fc1a1)`;
-    changeTheme.style.backgroundColor = black;
-    changeTheme.style.color = white;
-   }else{
-    body.style.backgroundColor = emptyItem;
-    mainBox.style.background = emptyItem;
-    changeTheme.style.backgroundColor = emptyItem;
-    changeTheme.style.color = emptyItem;
+   if (body.style.background == emptyItem) {
+      body.style.background = black;
+   } else {
+      body.style.background = emptyItem;
    }
-   changeTheme.style.border = 'none';
 })
 
 
 window.addEventListener('load', () => {
-  changeImg();
+   changeImg();
 });
